5f0c0b7eb22722dcd5d31eb51303e47758a9a523
Joshua <joshuageissler.pharworlds@gmail.com>
2025-01-22 22:20:29 +0100

    first respone

--- src/server.cpp
+++ src/server.cpp
@@ -57,10 +57,10 @@ int main(int argc, char **argv) {
 
   std::cout << "Waiting for a client to connect...\n";
 
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  server_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   const char* message = "HTTP/1.1 200 OK\r\n\r\n";
-  send(server_fd, "HTTP/1.1 200 OK\r\n\r\n", 20, 0);
+  send(server_fd, message, strlen(message), 0);
   close(server_fd);
 
   return 0;
